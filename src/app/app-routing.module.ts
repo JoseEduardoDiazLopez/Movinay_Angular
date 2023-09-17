ba818b620f077bd99afb747069077b34720a7fd0
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepci.edu.mx>
2023-09-16 22:42:58 -0700

    fixed components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { CitasCComponent } from './citas-c/citas-c.component';
+import { CitasCComponent } from './components/citas-c/citas-c.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'principal', component: HomeComponent},
-  { path: 'citasC', component: CitasCComponent }
+  {path: 'citasC', component: CitasCComponent },
+  {path: '**', redirectTo:'principal',pathMatch:"full"} // Si no existe la ruta, que todo lo retorne a principal
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { CitasCComponent } from './citas-c/citas-c.component';
+import { CitasCComponent } from './components/citas-c/citas-c.component';
 import { TabsComponent } from './tabs/tabs.component';
 
 
