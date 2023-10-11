e0f134789305be3b3842b26896480f3aef0ef460
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepci.edu.mx>
2023-10-10 23:57:27 -0700

    menu date

--- src/app/components/citas-c/citas-c.component.ts
+++ src/app/components/citas-c/citas-c.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit, Renderer2, ElementRef} from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import {fromEvent,of} from 'rxjs'
+import {map,startWith} from 'rxjs/operators'
 @Component({
   selector: 'app-citas-c',
   templateUrl: './citas-c.component.html',
   styleUrls: ['./citas-c.component.css']
 })
 export class CitasCComponent {
-
+  menu_icon_variable: boolean = false;
+  menuVariable: boolean = false;
+  openMenu() {
+    this.menuVariable =! this.menuVariable;
+    this.menu_icon_variable =! this.menu_icon_variable;
+  }
 }
