f4785778cc31c26a20cb9a110ca98945b99e58f4
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepci.edu.mx>
2023-10-17 23:02:15 -0700

    prueba mongo

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  menu_icon_variable: boolean = false;
-  menuVariable: boolean = false;
-  openMenu() {
-    this.menuVariable =! this.menuVariable;
-    this.menu_icon_variable =! this.menu_icon_variable;
-  }
 }
