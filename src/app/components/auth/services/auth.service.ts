567bd8ead580e6f56e366c44df4f341b60116403
KevinManjarrez <keazmanjarrezma@ittepic.edu.mx>
2023-10-20 01:22:35 -0600

    Continuación de cambios del compoenten login

--- src/app/components/auth/services/auth.service.ts
+++ src/app/components/auth/services/auth.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { UserI } from '../models/user';
+import { JwtResponseI } from '../models/jwt-response';
+import { tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  AUTH_SERVER: string = 'http://localhost:3000';
+  authSubject = new BehaviorSubject(false);
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  register(user: UserI): Observable<JwtResponseI> {
+    return this.httpClient
+      .post<JwtResponseI>(`${this.AUTH_SERVER}/register`, user)
+      .pipe(
+        tap((res: JwtResponseI) => {
+          if (res) {
+            // guardar token
+          }
+        })
+      );
+  }
+
+  login(user: UserI): Observable<JwtResponseI> {
+    return this.httpClient
+      .post<JwtResponseI>(`${this.AUTH_SERVER}/login`, user)
+      .pipe(
+        tap((res: JwtResponseI) => {
+          if (res) {
+            // guardar token
+          }
+        })
+      );
+  }
+
 }
