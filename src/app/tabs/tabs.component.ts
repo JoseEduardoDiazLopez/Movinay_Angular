9e921cab3ece3a6573fa01fcdea6b05a6120a586
Eduardo Díaz <joeddiazlo@ittepic.edu.mx>
2023-10-22 23:20:23 -0600

    citas funcional

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -27,7 +27,33 @@ export class TabsComponent {
     }, (error: any)=>{
       console.log(error);
     });
-  }// obtenercita
+  }// obtener cita
+
+  CrearCita(){
+    const moduloCita = (document.getElementById('cmbxModulo') as HTMLSelectElement)?.value;
+    const fecha = (document.getElementsByName('fecha')[0] as HTMLInputElement)?.value;
+    const horaCita = (document.getElementById('cmbxHora') as HTMLSelectElement)?.value;
+    const tipoTramite = (document.getElementById('cmbxTramite') as HTMLSelectElement)?.value;
+
+    const CITAS: Citas = {
+     
+    idCita : 1,
+    Fecha :fecha,
+    Hora :horaCita,
+    numTramite :tipoTramite,
+    idUsuario : 1,
+    idOficina : 1 
+    }
+   
+      console.log(CITAS);
+      this._CitasService.guardarCitas(CITAS).subscribe(data =>{
+        this.toastr.info('Se ha añadido una nueva cita','¡cita añadida!',);
+       
+      }, error =>{
+        console.log(error);
+    
+      })
+  }// crear cita
   
 }
 
